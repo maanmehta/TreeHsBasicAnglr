9a04d8f8ffdd117593e69df406649d3177a4f8ca
maanmehta <msmgrd-adm@yahoo.com>
2016-09-20 01:30:27 -0400

    changes to angular controller and services js files and json file

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,25 +1,68 @@
 'use strict';
 
-angular.module('todoListApp')
-.controller('mainCtrl', function($scope, dataService) {
+angular.module("todoListApp")
+
+.controller('mainCtrl', function ($scope, dataService) {
+    
+  $scope.helloWorld = function() {
+    console.log("Hello! This is helloWorld Ctrlr function, in the mainCtrl");
+  };
+    
+  $scope.helloConsole = dataService.helloWorld;
+    
+  $scope.deleteTodo = function(todo,idx) {
+      dataService.deleteTodo(todo,idx);
+      $scope.todos.splice(idx,1);
+  }
+  
   $scope.addTodo = function() {
-    var todo = {name: "This is a new todo."};
-    $scope.todos.push(todo);
-  };
+      var todo={name:'A new Todo'};
+      $scope.todos.push(todo);
+  }
+      
+  $scope.saveTodo = dataService.saveTodo;
+
+    
+//  $scope.todosOld =[
+//        {"name": "Clean house"},
+//        {"name": "Water plans"},
+//        {"name": "Weedman"},
+//        {"name": "Call Bank"},
+//        {"name": "Email Dave"}
+//  ]
   
-  $scope.helloWorld = dataService.helloWorld;
+  var cbFunction = function(response) { 
+      // response param is passed on by the $http.get.then method below in our service
+      
+      $scope.todos = response.data; //save the response json obj to our todos obj
+      
+      console.log("Response data object is:");
+      console.log(response.data);
+  }
   
-  dataService.getTodos(function(response) { 
-      console.log(response.data);  
-      $scope.todos = response.data;
-    });
-  
-  $scope.deleteTodo = function(todo, $index) {
-    dataService.deleteTodo(todo);
-    $scope.todos.splice($index, 1);
-  };
-  
-  $scope.saveTodo = function(todo) {
-    dataService.saveTodo(todo);
-  };
+  // execute the getTodos method of the service and pass in a callback function defined above
+  // that will executed upon success of get http method call
+  dataService.getTodos(cbFunction);
+    
 })
+
+
+//.controller('coolCtrl', function ($scope) {
+//    
+//  $scope.whoAmI = function() {
+//    console.log("Hello! This is whoAmI  function, in the coolCtrl");
+//  };
+//    
+//  $scope.helloWorld = function() {
+//    console.log("Hello! This is overridden helloWorld Ctrlr function, in the coolCtrl");
+//  };
+//    
+//})
+//
+//.controller('imASibling', function ($scope) {
+//
+//  $scope.foobar=1234;
+//
+//})
+
+;
--- scripts/services/data.js
+++ scripts/services/data.js
@@ -1,24 +1,29 @@
 'use strict';
 
-angular.module('todoListApp')
+angular.module("todoListApp")
+
 .service('dataService', function($http) {
-  this.helloWorld = function() {
-    console.log("This is the data service's method!!");
-  };
-  
-  this.getTodos = function(callback){
-    $http.get('mock/todos.json')
-    .then(callback)
-  };
-  
-  this.deleteTodo = function(todo) {
-    console.log("The " + todo.name + " todo has been deleted!")
-    // other logic
-  };
-  
-  this.saveTodo = function(todo) {
-    console.log("The " + todo.name + " todo has been saved!");
-    // other logic...
-  };
-  
-});
+    
+    this.helloWorld = function() {
+        console.log("This is helloWorld function from dataService service");
+    }
+    
+    this.getTodos = function(callbackFunc) {
+        $http.get('mock/todos.json')
+        .then(callbackFunc) //then method passes in the response object
+    }
+    
+    this.deleteTodo = function(todo) {
+        
+        console.log("deleteTodo service method - Call to DELETE the following todo --> "+todo.name);
+        //other logic to delete todo from backend
+    }
+    
+    this.saveTodo = function(todo) {
+        console.log("saveTodo service method - Call to SAVE the following todo --> "+todo.name);
+        //other logic to delete todo from backend
+    }
+      
+})
+
+;
